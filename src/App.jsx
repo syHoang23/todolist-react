a61410daed1695d10510980c09b3c15ef9e476d0
syHoang23 <equalforever123@gmail.com>
2025-05-19 14:48:15 +0700

    update backgound from white to gray 200

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import TodoList from "./Component2/TodoList";
 
 export default function App() {
   return (
-    <div className="bg-sky-50 p-9 rounded-2xl shadow-md text-center w-[400px]">
+    <div className="bg-gray-200 p-9 rounded-2xl shadow-md text-center w-[400px]">
       <TodoList />
     </div>
   );
