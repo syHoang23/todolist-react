eeaa58a4d7546a0399e4019323dd3d6c8c91e5b3
syHoang23 <equalforever123@gmail.com>
2025-05-22 22:44:34 +0700

    update position button

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -44,6 +44,11 @@ export default function Card({ id, value, onDelete }: Props) {
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m7.49 12-3.75 3.75m0 0 3.75 3.75m-3.75-3.75h16.5V4.499" />
                                 </svg>
                             </button>
+                            <div className='text-gray-200 rounded-2xl'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m7.49 12-3.75 3.75m0 0 3.75 3.75m-3.75-3.75h16.5V4.499" />
+                                </svg>
+                            </div>
                         </div>
                     </>
                 ) : (
